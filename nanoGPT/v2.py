9722a1a5bb3bc637a2aa5498a4e701c9b63402db
sandeepgangaram <100362009+sandeepgangaram@users.noreply.github.com>
2026-03-29 14:17:12 +0530

    increase eval interval

--- nanoGPT/v2.py
+++ nanoGPT/v2.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 5000
-eval_interval = 300
+eval_interval = 500
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
